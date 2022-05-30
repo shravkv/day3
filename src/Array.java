fc57aacf8bb309306c03c4f590c5629ef05ad8c3
sravan <sravan.k.vodnala@gmail.com>
2022-05-30 17:13:01 +0530

    Added array

---
+++ src/Array.java
@@ -0,0 +1,12 @@
+public class Array {
+
+    public static void main(String[] args) {
+        double [] myList = {1.5, 2.4, 3.2, 3.6 };
+
+        // print all the array elements
+        for(double element: myList) {
+            System.out.println(element);
+        }
+        }
+
+}
