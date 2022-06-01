4ca1aa0f1be52c1bc3e99504e9e2364bf1f0a44d
sravan <sravan.k.vodnala@gmail.com>
2022-06-01 17:25:22 +0530

    descending

---
+++ src/Descending.java
@@ -0,0 +1,20 @@
+public class Descending {
+
+    public static void main(String[] args) {
+        int[] num = {7,5,2,6,4};
+        int temp = 0;
+        System.out.println("Duplicate elements of an array");
+        for (int i = 0; i < num.length; i++) {
+            for (int j = i + 1; j < num.length; j++) {
+                if (num[i] < num[j]) {
+                    temp = num[i];
+                    num[i] = num[j];
+                    num[j] = temp;
+                }
+            }
+        }
+        for (int i = 0; i < num.length; i++) {
+            System.out.println(num[i]);
+        }
+    }
+}
